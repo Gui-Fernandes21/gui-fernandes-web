5de8d4a370ec0139e5ecf8df0d77e707c325a0ad
Guilherme Fernandes <guilherme.fernandes@cxpbrasil.com.br>
2023-12-05 09:12:19 +0100

    global styles setup

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <NuxtWelcome />
+    
   </div>
 </template>
+
+<style>
+:root {
+  --dark: #1F1F21;
+  --gray-900: #484848;
+  --gray-700: #727272;
+  --gray-500: #B1B1B1;
+  --gray-300: #D1D1D1;
+  --gray-100: #EDEDED;
+  --primary: #2AEA8C;
+  --primary-dark: #10B967;
+  --primary-light: #5DFCAE;
+}
+html {
+  background: var(--dark);
+}
+</style>
