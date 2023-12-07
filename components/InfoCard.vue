481641b9abb60c5c7d159f84d4069a17aa628f6e
Guilherme Fernandes <guilherme.fernandes@cxpbrasil.com.br>
2023-12-07 01:30:51 +0100

    infocard adding

--- components/InfoCard.vue
+++ components/InfoCard.vue
@@ -10,13 +10,25 @@
 				<h2 class="description-role"> Front-End Developer</h2>
 			</div>
 		</header>
+		<div class="img-wrapper">
+			<img src="/images/portfolio-pic-b&w.JPG?url" alt="">
+		</div>
 	</div>
 </template>
 
 <style scoped>
 header {
   display: flex;
-
+	justify-content: space-between;
+	/* background-color: #7fffd44e; */
+	margin: 1.3rem 2rem;
+	user-select: none;
+}
+img {
+	width: 209px;
+	height: 209px;
+	flex-shrink: 0;
+	border-radius: 36px;
 }
 .card {
 	width: 391px;
@@ -24,12 +36,10 @@ header {
 	overflow: hidden;
 	border-radius: 60px;
 	box-shadow: 0px 0px 9.3px 1px rgba(171, 171, 171, 0.25);
-	background: var(--Dark, #1f1f21);
+	background: var(--dark);
 }
-/* .name {
-  margin: 3rem;
-} */
 .description-role {
+	width: 4rem;
 	color: var(--gray-300);
 	text-align: center;
 	font-family: "Playfair", serif;
@@ -37,5 +47,10 @@ header {
 	font-style: normal;
 	font-weight: 400;
 	line-height: normal;
+}
+.img-wrapper {
+	padding: 1rem;
+	background-color: #7fffd44e;
+	text-align: center;
 }
 </style>
