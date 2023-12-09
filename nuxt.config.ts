e55286879044b5dcdbe0532eb1cd7766a84fcca8
Guilherme Fernandes <guilherme.fernandes@cxpbrasil.com.br>
2023-12-09 14:06:04 +0100

    scrollbar customization

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    'nuxt-icons'
-  ],
-  css: ['~/assets/css/main.css']
-})
+	devtools: { enabled: true },
+	modules: ["nuxt-icons"],
+	css: ["~/assets/css/main.css"],
+});
